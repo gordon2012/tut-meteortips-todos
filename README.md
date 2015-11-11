466667b2ec0ca2f371aff20f5a5b751fd1c7ced7
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2015-11-11 12:09:12 -0800

    complete section 'Refactored Rules'

--- README.md
+++ README.md
@@ -4,22 +4,22 @@ http://meteortips.com/second-meteor-tutorial
 
 ## Sections:
 
-1. Introduction
-1. Getting Started
+1. ~~Introduction~~
+1. ~~Getting Started~~
 	* ~~Project Setup~~
 	* ~~Listing Tasks~~
 	* ~~Refactoring Templates~~
 	* ~~Adding Tasks~~
 	* ~~Removing Tasks~~
-1. Managing Todos
-1. Routing, Part 1
-1. Routing, Part 2
-1. User Accounts
-1. Routing, Part 3
+1. ~~Managing Todos~~
+1. ~~Routing, Part 1~~
+1. ~~Routing, Part 2~~
+1. ~~User Accounts~~
+1. ~~Routing, Part 3~~
 1. Validation
 	* ~~stuff~~
 	* ~~Error Messages~~
-	* Refactored Rules
+	* ~~Refactored Rules~~
 	* Submit Handler
 	* Showing Errors
 1. Publish & Subscribe
--- client/app.js
+++ client/app.js
@@ -142,24 +142,30 @@ Template.register.events
 	'submit form': function(event)
 	{
 		event.preventDefault();
-		var email = $('[name=email]').val();
-		var password = $('[name=password]').val();
-		Accounts.createUser
-		({
-			email: email,
-			password: password
-		}, function(error)
-		{
-			if (error)
-			{
-				console.log(error.reason)
-			}
-			else
-			{
-				Router.go("home");
-			}
-		});
+
+		// var email = $('[name=email]').val();
+		// var password = $('[name=password]').val();
+		// Accounts.createUser
+		// ({
+		// 	email: email,
+		// 	password: password
+		// }, function(error)
+		// {
+		// 	if (error)
+		// 	{
+		// 		console.log(error.reason)
+		// 	}
+		// 	else
+		// 	{
+		// 		Router.go("home");
+		// 	}
+		// });
 	}
+});
+
+Template.register.onRendered(function()
+{
+	$('.register').validate();
 });
 
 
@@ -214,36 +220,7 @@ Template.login.onCreated(function()
 
 Template.login.onRendered(function()
 {
-	//console.log("The 'login' template was just rendered.");
-	$('.login').validate
-	({
-		rules:
-		{
-			email:
-			{
-				required: true,
-				email: true
-			},
-			password:
-			{
-				required: true,
-				minlength: 6
-			}
-		},
-		messages:
-		{
-			email:
-			{
-				required: "You must enter an email address.",
-				email: "You have entered an invalid email address."
-			},
-			password:
-			{
-				required: "You must enter a password.",
-				minlength: "Your password must be at least {0} characters."
-			}
-		}
-	});
+	$('.login').validate();
 });
 
 Template.login.onDestroyed(function()
@@ -254,9 +231,37 @@ Template.login.onDestroyed(function()
 
 
 
+// ======== DEFAULTS ========
 
-
-
+$.validator.setDefaults
+({
+	rules:
+	{
+		email:
+		{
+			required: true,
+			email: true
+		},
+		password:
+		{
+			required: true,
+			minlength: 6
+		}
+	},
+	messages:
+	{
+		email:
+		{
+			required: "You must enter an email address.",
+			email: "You've entered an invalid email address."
+		},
+		password:
+		{
+			required: "You must enter a password.",
+			minlength: "Your password must be at least {0} characters."
+		}
+	}
+});
 
 
 
